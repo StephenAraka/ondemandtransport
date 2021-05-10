c353a9199c1a2f2c8da421634e7ea6c07accd99c
Timmutegi <mutegi.timothy538@gmail>
2021-05-10 17:56:32 +0200

    Added routes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -9,15 +9,19 @@ import store from './redux/store';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import LandingPage from './components/LandingPage';
+import ZonesPage from './pages/ZonesPage';
+import RoutesPage from './pages/RoutesPage';
+import DriversPage from './pages/DriversPage';
+import BusstopsPage from './pages/BusstopsPage';
 
 // Protected route should have token. If not, login.
-const ProtectedRoute = ({ isAllowed, ...props }) => (
-  isAllowed
-    ? <Route {...props} />
-    : <Redirect to="/login" />
-);
+// const ProtectedRoute = ({ isAllowed, ...props }) => (
+//   isAllowed
+//     ? <Route {...props} />
+//     : <Redirect to="/login" />
+// );
 
-const hasToken = store.getState().user.accessToken;
+// const hasToken = store.getState().user.accessToken;
 
 const Routes = () => {
   return (
@@ -26,6 +30,10 @@ const Routes = () => {
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/login" component={LoginPage} />
         <Route exact path="/register" component={RegisterPage} />
+        <Route exact path="/zones" component={ZonesPage} />
+        <Route exact path="/drivers" component={DriversPage} />
+        <Route exact path="/busstops" component={BusstopsPage} />
+        <Route exact path="/routes" component={RoutesPage} />
         {/* <ProtectedRoute isAllowed={hasToken} exact path="/" component={App} /> */}
       </Switch>
     </Router>
